3adce36fe28720a6ea76c0235d8bb646cac03cac
yangLi <yang.li@anchora.me>
2017-07-26 16:08:27 +0800

    weather test go back

--- wx.py
+++ wx.py
@@ -6,8 +6,6 @@ from flask import make_response
 import time
 import xml.etree.ElementTree as ET
 import re
-import json
-import requests
 
 app = Flask(__name__)
 
@@ -85,53 +83,14 @@ def wechat_auth():
                 )
                 return reply
             elif re_result:
-                express_no = content[2:]
-                query_url = "https://biz.trace.ickd.cn/auto/" + express_no + "?mailNo=" + express_no + "&spellName=&exp-textName=&ts=123456&enMailNo=123456789&_1500966824578="
-                result = requests.get(query_url)
-                result_text = result.text[7:-1]
-                json_text = json.loads(result_text)
-                final_text = ''
-                for i in json_text['data']:
-                    final_text = final_text + i['time'] + ":" + i['context'] + '\n'
-                reply = reply_xml % (
-                    fromUserName,
-                    toUserName,
-                    createTime,
-                    'text',
-                    final_text
-                )
-                return reply
-            elif content == '嘉兴天气':
-                query_url = "http://tj.nineton.cn/Heart/index/all?city=CHZJ020000&language=&unit=&aqi=&alarm=&key="
-                result = requests.get(query_url)
-                result_text = result.text
-                json_text = json.loads(result_text)
-                final_text = ''
-                now_weather = json_text['weather'][0]['now']
-                future_weather = json_text['weather'][0]['future']
-                reply_str = u"今日天气: " + now_weather['text'] + "\n" + u"温度: " + now_weather['temperature'] + "\n" + u"体感温度: " + \
-                    now_weather[
-                        'feels_like'] + "\n" + u"湿度: " + \
-                    now_weather['humidity'] + "\n" + u"pm2.5: " + now_weather['air_quality']['city'][
-                        'pm25'] + "\n" + u"空气质量: " + \
-                    now_weather['air_quality']['city']['quality'] + "\n\n\n"
-
-                for i in future_weather:
-                    date = i['date']
-                    day = i['day']
-                    text = i['text']
-                    high = i['high']
-                    low = i['low']
-                    reply_str = reply_str + date + '\n' + day + ':' + text + '\n' + u"温度" + low + '-' + high + '\n\n'
 
                 reply = reply_xml % (
                     fromUserName,
                     toUserName,
                     createTime,
                     'text',
-                    reply_str
+                    u'测试一下'
                 )
-                return reply
             else:
                 reply = reply_xml % (
                     fromUserName,
